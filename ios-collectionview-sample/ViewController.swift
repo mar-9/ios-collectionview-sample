fce708177e23803448d8fcfff6c1381107f48b6b
MASANORI SONE <>
2021-04-21 21:20:17 +0900

    add new files

---
+++ ios-collectionview-sample/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  ios-collectionview-sample
+//
+//  Created by mar 100s on 2021/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController ,UICollectionViewDataSource, UICollectionViewDelegate  {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let photos = ["image1", "image1","image1","image1","image1","image1","image1","image1","image1","image1","image1","image1","image1","image1"]
+    
+    let columnLayout = ColumnFlowLayout(
+        cellsPerRow: 3,
+        minimumInteritemSpacing: 10,
+        minimumLineSpacing: 10,
+        sectionInset: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.collectionViewLayout = columnLayout
+        collectionView.contentInsetAdjustmentBehavior = .always
+
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        // "Cell" はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+                                               for: indexPath)
+        
+        // Tag番号を使ってImageViewのインスタンス生成
+        let imageView = testCell.contentView.viewWithTag(1) as! UIImageView
+        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
+        let cellImage = UIImage(named: photos[indexPath.row])
+        // UIImageをUIImageViewのimageとして設定
+        imageView.image = cellImage
+        
+        // Tag番号を使ってLabelのインスタンス生成
+        let label = testCell.contentView.viewWithTag(2) as! UILabel
+        label.text = photos[indexPath.row]
+        
+        return testCell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // section数は１つ
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
+        return photos.count;
+    }
+}
+
